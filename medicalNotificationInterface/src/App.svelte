a3967ffaf25eb360d6a810b60dfd013b3151f7f0
Zachary Brown <brownz4@mail.uc.edu>
2024-12-07 16:21:02 -0500

    setup

---
+++ medicalNotificationInterface/src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+  import Header from './lib/Header.svelte'
+  import './app.css';
+</script>
+
+<main>
+  <Header />
+
+  <div class="content-wrapper">
+   <div class="leftColumn">
+      left
+    </div>
+
+    <div class="rightColumn">
+      right
+    </div>
+  </div>
+</main>
+
+<style>
+  @import './colors.css';
+  
+
+  main {
+    background: var( --background);
+    color: var( --text);
+  }
+  
+  .content-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px dotted rgb(96 139 169);
+    gap: 5vw;
+    padding: 2vw 4vw;
+
+  }
+
+  .leftColumn {
+    
+  }
+
+  .rightColumn {
+  
+  }
+
+  
+</style>
---
+++ medicalNotificationInterface/src/lib/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let title = "Default Text";
+</script>
+
+<div class="header">
+  {title}
+</div>
+
+<style>
+  @import '../colors.css';
+
+  .header {
+    background: var( --red);
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    padding: 2vh;
+  }
+</style>
